06efe3f34ec4f94cdec49349986515a227f145ce
dmbrson <dka04@list.ru>
2024-09-10 17:39:44 +1000

    изменение работы сереализатора. добавление обработчика ошибок в post-запросах

--- drfsite/electricity/serializers.py
+++ drfsite/electricity/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 from .models import *
-
-class InstallationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Installation
-        fields = ('title', 'cat_id')
+class InstallationSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
--- drfsite/electricity/views.py
+++ drfsite/electricity/views.py
@@ -1,8 +1,22 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import *
 from .serializers import InstallationSerializer
 
 
-class InstallationAPIView(generics.ListAPIView):
-    queryset = Installation.objects.all()
-    serializer_class = InstallationSerializer
+class InstallationAPIView(APIView):
+    def get(self, request):
+        w = Installation.objects.all()
+        return Response({'posts': InstallationSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = InstallationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Installation.objects.create(
+            title = request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id'],
+        )
