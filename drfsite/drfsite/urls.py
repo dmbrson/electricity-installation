2112c2eaddcecfb6f07dba366bb17a487db6efdb
dmbrson <dka04@list.ru>
2024-09-12 17:40:20 +1000

    Замена APIView на ListCreateAPIView и UpdateAPIView для упрощения кода. А также в сереализаторе замененен класс на ModelSerializer для упрощения кода

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from electricity.views import InstallationAPIView
+from electricity.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/installationlist/', InstallationAPIView.as_view()),
-    path('api/v1/installationlist/<int:pk>/', InstallationAPIView.as_view()),
+    path('api/v1/installationlist/', InstallationAPIList.as_view()),
+    path('api/v1/installationlist/<int:pk>/', InstallationAPIUpdate.as_view()),
 ]
--- drfsite/electricity/views.py
+++ drfsite/electricity/views.py
@@ -6,31 +6,38 @@ from .models import *
 from .serializers import InstallationSerializer
 
 
-class InstallationAPIView(APIView):
-    def get(self, request):
-        w = Installation.objects.all()
-        return Response({'posts': InstallationSerializer(w, many=True).data})
+class InstallationAPIList(generics.ListCreateAPIView):
+    queryset = Installation.objects.all()
+    serializer_class = InstallationSerializer
 
-    def post(self, request):
-        serializer = InstallationSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response({'posts': serializer.data})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
-        try:
-            instance = Installation.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exists"})
-
-        serializer = InstallationSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"post": serializer.data})
+class InstallationAPIUpdate(generics.UpdateAPIView):
+    queryset = Installation.objects.all()
+    serializer_class = InstallationSerializer
+# class InstallationAPIView(APIView):
+#     def get(self, request):
+#         w = Installation.objects.all()
+#         return Response({'posts': InstallationSerializer(w, many=True).data})
+#
+#     def post(self, request):
+#         serializer = InstallationSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#
+#         return Response({'posts': serializer.data})
+#
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method PUT not allowed"})
+#         try:
+#             instance = Installation.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exists"})
+#
+#         serializer = InstallationSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"post": serializer.data})
     # def delete(self, request, *args, **kwargs):
     #     pk = kwargs.get("pk", None)
     #     if not pk:
